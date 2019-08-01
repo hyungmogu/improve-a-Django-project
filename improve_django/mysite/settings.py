77d82a312a52602f83cd163efb73a30f21e52212
Hyungmo Gu <guhyungm7@gmail.com>
2019-07-31 23:19:49 -0700

    Setup Django Toolbar

--- improve_django/mysite/settings.py
+++ improve_django/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'menu',
 )
 
@@ -49,6 +50,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'mysite.urls'
@@ -71,6 +73,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
+INTERNAL_IPS = ['127.0.0.1', '::1', '0.0.0.0']
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
