edf86e6540981e2cdcb74471e244f2aaa55e5b17
Hyungmo Gu <guhyungm7@gmail.com>
2019-08-01 08:11:49 -0700

    style: Add custom error validation messages

--- improve_django/menu/forms.py
+++ improve_django/menu/forms.py
@@ -4,7 +4,13 @@ from django.forms.extras.widgets import SelectDateWidget
 from .models import Menu, Item, Ingredient
 
 class MenuForm(forms.ModelForm):
-
+    items = forms.ModelMultipleChoiceField(
+        queryset=Item.objects,
+        error_messages={'required': 'Please select at least one item'}
+    )
+    expiration_date = forms.DateTimeField(
+        error_messages={'invalid': 'Please enter date in the format MM/DD/YYYY'}
+    )
     class Meta:
         model = Menu
         exclude = ('created_date',)
