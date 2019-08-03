a991b8503e94a555d99508da92d3f6912c4e58c1
Hyungmo Gu <guhyungm7@gmail.com>
2019-08-03 04:43:13 -0700

    test: Add tests for 'sign_up/' page  (POST)

--- improve_django/accounts/tests.py
+++ improve_django/accounts/tests.py
@@ -2,6 +2,12 @@
 TODO LIST FOR ACCOUNTS APP (TESTS)
 
 []: 1) Add test for user model
+    - given 2 users, with 1 of format {username:"hello", password: "hello"}, and the other
+    {username: "world", password: "world"},
+        []: user model returns query count of 2
+        []: user model with pk == 1 has name "hello" and password "hello"
+        []: user model with pk == 2 has name "world" and password "hello"
+        []: type casting a queried item returns username
 
 [x]: 2) Add test for /accounts/login
     GET REQUEST
@@ -14,17 +20,17 @@ TODO LIST FOR ACCOUNTS APP (TESTS)
         [x]: When not successful, and username is incorrect, user is sent back to sign_in_page
         [x]: When successful, user is redirected to home page
 
-[]: 3) Add test for /accounts/sign_up
+[x]: 3) Add test for /accounts/sign_up
     GET REQUEST
         [x]: returns status 200 on visit
         [x]: uses layout.html as template
         [x]: uses sign_up.html as template
 
     POST REQUEST
-        []: When unsuccessful, user is sent back to sign up page
-        []: When successful, message "You're now a user! You've been signed in, too." is returned
-        []: When successful, user model has total query count of 1
-        []: When successful, user is redirected to home page
+        [x]: When unsuccessful, user is sent back to sign up page
+        [x]: When successful, message "You're now a user! You've been signed in, too." is returned
+        [x]: When successful, user model has total query count of 1
+        [x]: When successful, user is redirected to home page
 
 [x]: 4) Add test for /accounts/logout
     [x]: When successful, user is redirected to home page
@@ -37,6 +43,7 @@ from django.core.urlresolvers import reverse
 
 # Create your tests here.
 
+# VIEW TESTS
 class LoginPageGETRequestTestCase(TestCase):
     def setUp(self):
         self.resp = self.client.get(reverse('accounts:login'))
@@ -122,6 +129,58 @@ class SignUpGETRequestTestCase(TestCase):
         self.assertTemplateUsed(self.resp, expected)
 
 
+class SignUpPOSTRequestTestCase(TestCase):
+    def setUp(self):
+        self.resp = self.client.post(reverse('accounts:sign_up'), {
+            'username': 'hello',
+            'password1': 'hello1',
+            'password2': 'hello1'
+        })
+
+    def test_return_to_sign_up_page_if_signup_unsuccessful(self):
+        expected = 'accounts/sign_up.html'
+
+        response1 = self.client.post(reverse('accounts:sign_up'), {
+            'username': 'hello',
+            'password1': 'hello1',
+            'password2': 'hello2'
+        })
+
+        response2 = self.client.post(reverse('accounts:sign_up'), {
+            'username': 'hel lo',
+            'password1': 'hello1',
+            'password2': 'hello1'
+        })
+
+
+        self.assertTemplateUsed(response1, expected)
+        self.assertTemplateUsed(response2, expected)
+
+    def test_return_message_if_signup_successful(self):
+        expected = "You're now a user! You've been signed in, too."
+
+        response = self.client.post(reverse('accounts:sign_up'), {
+            'username': 'hello',
+            'password1': 'hello1',
+            'password2': 'hello1'
+        }, follow=True)
+
+        messages = list(response.context['messages'])
+        result = str(messages[0])
+
+        self.assertEqual(expected, result)
+
+    def test_return_user_model_with_count_of_1_if_signup_successful(self):
+        expected = 1
+
+        result = User.objects.all().count()
+
+        self.assertEqual(expected, result)
+
+    def test_return_to_home_page_if_signup_successful(self):
+        self.assertRedirects(self.resp, reverse('home'), fetch_redirect_response=False)
+
+
 class LogoutPageTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user('hello', 'hello@example.com', 'hello')
@@ -140,7 +199,6 @@ class LogoutPageTestCase(TestCase):
         self.assertEqual(expected, result)
 
     def test_return_home_page_if_logout_successful(self):
-        result = self.res_logout.status_code
         self.assertRedirects(self.res_logout, reverse('home'), fetch_redirect_response=False)
 
     def test_return_message_if_logout_successful(self):
